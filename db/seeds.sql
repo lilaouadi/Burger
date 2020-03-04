07fa681cd57e970fad8b56faefe3c6ed64b6fe17
Lila Ouadi <lilaouadi@Lilas-Air.attlocal.net>
2020-03-04 12:36:00 -0800

    added sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('chickenburger');
+INSERT INTO burgers  (burger_name) VALUES ('veggieburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('doublecheeseburger', true);
+INSERT INTO burgers   (burger_name, devoured) VALUES ('MacBurger', true);
+INSERT INTO burgers  (burger_name, devoured) VALUES ('cheeseburger', true);
+INSERT INTO burgers  (burger_name )VALUES ('Owens');
